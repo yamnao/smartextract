d1033a4429ba30f3929cf4e5cec5b304e73694c9
Yamna O. <85222814+yamnao@users.noreply.github.com>
2024-11-26 15:40:58 +0000

    Update the size of the plot

--- R/plot_metadata_extract.R
+++ R/plot_metadata_extract.R
@@ -17,7 +17,7 @@ f_plot_log_file <- function(log_file){
     ggplot2::coord_polar(theta = "y") +
     ggplot2::theme_void() +
     ggplot2::geom_text(ggplot2::aes(label = label), position = ggplot2::position_stack(vjust = 0.5)) +
-    ggplot2::labs(title = "Comparison of SMART Surveys per Type", fill="Type of Surveys") +
+    ggplot2::labs(title = "Analysis of SMART surveys by type.", fill="Type of Surveys") +
     ggplot2::scale_fill_manual(values = c("#56B4E9", "#009E73", "#F0E442")) # Adjust colors if needed
 
   # Calculate proportions for Plot 2
@@ -32,14 +32,15 @@ f_plot_log_file <- function(log_file){
     ggplot2::coord_polar(theta = "y") +
     ggplot2::theme_void() +
     ggplot2::geom_text(ggplot2::aes(label = label), position = ggplot2::position_stack(vjust = 0.5)) +
-    ggplot2::labs(title = "Comparison of SMART Surveys with and Without Issues", fill = "Issue Status") +
+    ggplot2::labs(title = "Percentage of issues raised when extracting SMART contents.", fill = "Issues found:") +
     ggplot2::scale_fill_manual(values = c("FALSE" = "#56B4E9", "TRUE" = "#E69F00"))
 
+  log_file$min_year_survey <- as.numeric(lubridate::year(as.Date(paste0(log_file$min_year_survey, '/', log_file$min_month_survey, '/', log_file$min_day_survey))))
   plot3 <- ggplot2::ggplot(data = log_file, ggplot2::aes(min_year_survey))+
     ggplot2::geom_histogram(fill="#009E73")+
     ggplot2::theme_bw() +
     ggplot2::labs(x=ggplot2::element_blank(), y=ggplot2::element_blank(),
-                  title='Comparison of SMART Surveys per year')
+                  title='Analysis of SMART survey content extracted per year.')
 
   plot4 <- cowplot::plot_grid(plot1, plot2, plot3, labels = "AUTO")
   return(plot4)
